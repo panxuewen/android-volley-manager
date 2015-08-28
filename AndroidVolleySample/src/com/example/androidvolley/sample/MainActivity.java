235218f78a6d9fa40596ad852d75ea51bee4ae01
panxw{ } <panxw@users.noreply.github.com>
2015-08-28 20:19:17 +0800

    Update MainActivity.java

--- AndroidVolleySample/src/com/example/androidvolley/sample/MainActivity.java
+++ AndroidVolleySample/src/com/example/androidvolley/sample/MainActivity.java
@@ -114,7 +114,7 @@ public class MainActivity extends Activity {
 	};
 
 	/**
-	 * 因ImageLoader将回调对象存放在了WeakReference中，所以这里要将ImageListener回调设置成类对象
+	 * 因RequestManager将回调对象存放在了WeakReference中，所以这里要将RequestListener回调设置成类对象
 	 */
 	private RequestListener requestListener = new RequestListener() {
 		@Override
