0aa3fe35772d165a604e0efb9f81f65b874717bb
panxw <panxw@163.com>
2015-10-22 15:40:45 +0800

    reset minisdk

--- AndroidVolleySample/src/com/example/androidvolley/sample/NetworkApplication.java
+++ AndroidVolleySample/src/com/example/androidvolley/sample/NetworkApplication.java
@@ -2,8 +2,11 @@ package com.example.androidvolley.sample;
 
 import com.android.http.RequestManager;
 import com.android.volley.toolbox.ImageLoader;
-
+import com.android.volley.toolbox.ImageLoader.ImageCache;
+import android.annotation.SuppressLint;
 import android.app.Application;
+import android.graphics.Bitmap;
+import android.util.LruCache;
 
 public class NetworkApplication extends Application {
 
@@ -18,10 +21,44 @@ public class NetworkApplication extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		
+
 		RequestManager.getInstance().init(NetworkApplication.this);
 		sImageLoader = new ImageLoader(RequestManager.getInstance()
 				.getRequestQueue(), imageCacheMap);
+	}
+
+	@SuppressLint("NewApi")
+	public static class NetworkImageCache extends LruCache<String, Bitmap>
+			implements ImageCache {
+
+		public NetworkImageCache() {
+			this(getDefaultLruCacheSize());
+		}
+
+		public NetworkImageCache(int sizeInKiloBytes) {
+			super(sizeInKiloBytes);
+		}
+
+		@Override
+		protected int sizeOf(String key, Bitmap value) {
+			return value.getRowBytes() * value.getHeight() / 1024;
+		}
+
+		@Override
+		public Bitmap getBitmap(String url) {
+			return get(url);
+		}
+
+		@Override
+		public void putBitmap(String url, Bitmap bitmap) {
+			put(url, bitmap);
+		}
+
+		public static int getDefaultLruCacheSize() {
+			final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+			final int cacheSize = maxMemory / 8;
+			return cacheSize;
+		}
 	}
 
 }
